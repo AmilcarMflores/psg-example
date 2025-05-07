21977ec5a3de5662fc2163833274714f7153fe6a
Amilcar M. Flores <amilcarmflores@gmail.com>
2025-05-06 23:03:49 -0300

    Diálogo con michi

---
+++ retos_sesion_03/ejercicio_02.py
@@ -0,0 +1,10 @@
+# La persona se llama Amelia y el gato, Lucki
+
+print("Amelia: Hola Lucki :)")
+print("Lucki: Miau... ya era hora que me hables")
+print("Amelia: Jajaja, ¿tienes hambre otra vez?")
+print("Lucki: Siempre tengo hambre. ¿Tú no?")
+print("Amelia: ¿Por qué duermes encima de mi laptop?")
+print("Lucki: Está calientita, y además así no trabajas y me haces caso.")
+print("Amelia: Bueno, ya está bien por hoy. Me voy.")
+print("Lucki: Miau, no tardes. Lleva comida.")
