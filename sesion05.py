b044a872fc9324c4d4d14d855a839e699276b5b7
Amilcar M. Flores <amilcarmflores@gmail.com>
2025-05-17 16:42:45 -0300

    Clase sesión 05

---
+++ sesion05.py
@@ -0,0 +1,68 @@
+# print(type(1.1))
+
+# # Declarar usando la función int
+# variable_2 = int(20.0) # 20
+# print(variable_2)
+# print(type(variable_2))
+
+# # Valor en otras bases
+# print(0b1010)
+# print(0o12)
+# print(0xa)
+
+# variable_3 = 12345
+# print (variable_3)
+# print (type (variable_3))
+
+# # Flotante
+# print(float(2))
+
+# variable_5 = 0.99999999999999999
+# print(variable_5)
+# print ( type (variable_5) )
+
+# # Operaciones aritméticas
+# a = 10
+# b = 3
+# # Suma
+# print ("Suma")
+# print (a + b)
+# # Resta
+# print ("Resta")
+# print (a - b)
+# # Multiplicación
+# print ("Multiplicación")
+# print (a * b)
+# # División
+# print ("División")
+# print (a / b)
+# # Potencia
+# print ("Potencia")
+# print (a ** b)
+# # Módulo o residuo
+# print ("Módulo o residuo")
+# print (a % b)
+# # División entera
+# print ("División entera")
+# print (a // b)
+
+# Ejercicio de clase 😀
+con_min = 300
+sum = 3600
+hr_min = 60
+
+sum_min = sum / 60
+
+sum_tot = con_min + sum_min
+resl = sum_tot / 60
+print(resl)
+
+print ("Operadores de comparación con int - float")
+entero = 10
+flotante = 10.0
+print (entero < flotante)
+print (entero > flotante)
+print (entero == flotante)
+print (entero <= flotante)
+print (entero >= flotante)
+print (entero != flotante)
