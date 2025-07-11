a5940ad3d4376d323652ccdb875278fed505f416
Amilcar M. Flores <amilcarmflores@gmail.com>
2025-07-11 14:19:17 -0400

    Corrección - Diálogo con michi

--- retos_sesion_03/ejercicio_02.py
+++ retos_sesion_03/ejercicio_02.py
@@ -1,10 +1,10 @@
-# La persona se llama Amelia y el gato, Lucki
+# Diálogo entre Amelia y su gato Lucki
 
 print("Amelia: Hola Lucki :)")
-print("Lucki: Miau... ya era hora que me hables")
+print("Lucki: Miaw")
 print("Amelia: Jajaja, ¿tienes hambre otra vez?")
-print("Lucki: Siempre tengo hambre. ¿Tú no?")
+print("Lucki: Miaw")
 print("Amelia: ¿Por qué duermes encima de mi laptop?")
-print("Lucki: Está calientita, y además así no trabajas y me haces caso.")
+print("Lucki: Miaw")
 print("Amelia: Bueno, ya está bien por hoy. Me voy.")
-print("Lucki: Miau, no tardes. Lleva comida.")
+print("Lucki: Miaw")
