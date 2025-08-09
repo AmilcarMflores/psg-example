d90bc002d2e9dcf30c2b1d66c96ded3d7388bb5a
Amilcar M. Flores <amilcarmflores@gmail.com>
2025-08-09 18:24:39 -0400

    Sesión Bonus - Última sesión

---
+++ exercise.py
@@ -0,0 +1,13 @@
+maiz  trigo * 2 + soya
+
+0 * 1 * 2 + 1 -> 1 -> trigo
+
+maiz + trigo + soya - 2
+0   1   1   -2 -> 0
+
+
+
+1   0   0   *2 -> 
+
+
+
---
+++ sesion_bonus.py
@@ -0,0 +1,21 @@
+# sesion_bonus.py
+def suma(a, b):
+    c = a + b
+    return c
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(x, y):
+    z = x * y
+    return z
+
+def division(x, y):
+    return x / y
+
+a = 10
+b = 5
+print(suma(a, b))
+print(resta(a, b))
+print(multiplicacion(11, 5))
+print(division(a, b))
