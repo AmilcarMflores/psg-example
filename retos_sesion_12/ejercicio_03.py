4fce14a85e2a5a0e60c1633c8fa243d5c300d246
Amilcar M. Flores <amilcarmflores@gmail.com>
2025-08-15 16:50:22 -0400

    Corrección: Reemplazó de if's con con el método de conjuntos (intersección)

--- retos_sesion_12/ejercicio_03.py
+++ retos_sesion_12/ejercicio_03.py
@@ -1,30 +1,7 @@
 autos_jhon = {'Ferrari', 'Lamborghini', 'Porsche', 'Bugatti', 'McLaren'}
 autos_jess = {'Ferrari', 'Lamborghini', 'Tesla', 'Ford', 'Chevrolet'}
 
-autos_en_comun = set()
-
-if 'Ferrari' in autos_jhon and 'Ferrari' in autos_jess:
-    autos_en_comun.add('Ferrari')
-
-if 'Lamborghini' in autos_jhon and 'Lamborghini' in autos_jess:
-    autos_en_comun.add('Lamborghini')
-
-if 'Porsche' in autos_jhon and 'Porsche' in autos_jess:
-    autos_en_comun.add('Porsche')
-
-if 'Bugatti' in autos_jhon and 'Bugatti' in autos_jess:
-    autos_en_comun.add('Bugatti')
-
-if 'McLaren' in autos_jhon and 'McLaren' in autos_jess:
-    autos_en_comun.add('McLaren')
-
-if 'Tesla' in autos_jhon and 'Tesla' in autos_jess:
-    autos_en_comun.add('Tesla')
-
-if 'Ford' in autos_jhon and 'Ford' in autos_jess:
-    autos_en_comun.add('Ford')
-
-if 'Chevrolet' in autos_jhon and 'Chevrolet' in autos_jess:
-    autos_en_comun.add('Chevrolet')
+#autos_en_comun = autos_jhon.intersection(autos_jess)
+autos_en_comun = autos_jhon & autos_jess
 
 print(f"Tienen {len(autos_en_comun)} autos en común: {autos_en_comun}")
